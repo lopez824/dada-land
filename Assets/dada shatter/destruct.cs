c53325c1b8f0bb0a475f59e40176d533683c2744
u1372950 <u1372950@umail.utah.edu>
2021-11-01 16:12:27 -0600

    click and shatter effect

---
+++ Assets/dada shatter/destruct.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destruct : MonoBehaviour {
+
+	public GameObject destroy;
+	
+	void OnMouseDown()
+	{
+		Instantiate(destroy, transform.position, transform.rotation);
+		Destroy(gameObject);
+	}
+}
