3facfd033879b96d0a1148c20199a4b5cacfed44
lopez824 <35384909+lopez824@users.noreply.github.com>
2021-11-04 11:24:29 -0600

    Cleaned up scene and edited dada prefabs

--- Assets/dada shatter/destruct.cs
+++ Assets/dada shatter/destruct.cs
@@ -5,29 +5,31 @@ using UnityEngine;
 public class destruct : MonoBehaviour {
 
 	public GameObject destroy;
-	
-	//void OnTriggerEnter()
-	//{
-	//	Instantiate(destroy, transform.position, transform.rotation);
-	//	Destroy(gameObject);
-	//}
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag == "Weapon")
+    //    {
+    //        Instantiate(destroy, transform.position, transform.rotation);
+    //        Destroy(gameObject);
+    //    }
+    //}
 
-	void OnMouseDown()
-	{
-		Instantiate(destroy, transform.position, transform.rotation);
-		Destroy(gameObject); 
-		//Debug.Log("destroy");
+    void OnMouseDown()
+    {
+        Instantiate(destroy, transform.position, transform.rotation);
+        Destroy(gameObject);
+        //Debug.Log("destroy");
 
-		//StartCoroutine(respawn());
-	}
+        //StartCoroutine(respawn());
+    }
 
-	//public IEnumerator respawn()
- //   {
-	//	Debug.Log("respawn");
-	//	yield return new WaitForSeconds(2f);
-	//	Instantiate(destroy, transform.position, transform.rotation);
+    //public IEnumerator respawn()
+    //   {
+    //	Debug.Log("respawn");
+    //	yield return new WaitForSeconds(2f);
+    //	Instantiate(destroy, transform.position, transform.rotation);
 
-	//}
+    //}
 
-	
+
 }
