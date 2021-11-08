de15526a6863397f55b7ecf865ce9364ec09ee38
u1372950 <u1372950@umail.utah.edu>
2021-11-08 13:16:39 -0700

    .

--- Assets/lightadjust.cs
+++ Assets/lightadjust.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ligthadjust : MonoBehaviour
+public class lightadjust : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public Light mainlight;
+    
     void Start()
     {
+        InvokeRepeating("changeposition",0,2);
         
     }
 
@@ -14,5 +18,22 @@ public class ligthadjust : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void changeposition()
+    {
+
+        
+        this.transform.position = new Vector3(0.555999994f, -5.75f, -0.356999993f);
+        //this.transform.position = new Vector3(0.555999994f, 1.30700004f, -0.356999993f);
+        InvokeRepeating("change", 0, 2);
+    }
+
+    void change()
+    {
+
+        
+        //this.transform.position = new Vector3(0.555999994f, -5.75f, -0.356999993f);
+        this.transform.position = new Vector3(0.555999994f, 1.30700004f, -0.356999993f);
     }
 }
