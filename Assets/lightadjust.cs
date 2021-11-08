6232afaae73ad23b777e7a1f71943ac34f2fea71
u1372950 <u1372950@umail.utah.edu>
2021-11-08 13:44:31 -0700

    .

--- Assets/lightadjust.cs
+++ Assets/lightadjust.cs
@@ -7,10 +7,11 @@ public class lightadjust : MonoBehaviour
     // Start is called before the first frame update
 
     public Light mainlight;
+    public bool c=true;
     
     void Start()
     {
-        InvokeRepeating("changeposition",0,2);
+        Invoke("changeposition",2);
         
     }
 
@@ -22,18 +23,23 @@ public class lightadjust : MonoBehaviour
 
     void changeposition()
     {
-
         
-        this.transform.position = new Vector3(0.555999994f, -5.75f, -0.356999993f);
-        //this.transform.position = new Vector3(0.555999994f, 1.30700004f, -0.356999993f);
-        InvokeRepeating("change", 0, 2);
-    }
+        
+        if(c)
+        {
+            this.transform.position = new Vector3(0.555999994f, -5.75f, -0.356999993f);
+        }
 
-    void change()
-    {
-
+        //if(c!)
+        //{
+        //    this.transform.position = new Vector3(0.555999994f, 1.30700004f, -0.356999993f);
+        //    c = false;
+        //}
         
         //this.transform.position = new Vector3(0.555999994f, -5.75f, -0.356999993f);
-        this.transform.position = new Vector3(0.555999994f, 1.30700004f, -0.356999993f);
+        //this.transform.position = new Vector3(0.555999994f, 1.30700004f, -0.356999993f);
+        //InvokeRepeating("change", 0, 2);
     }
+
+    
 }
---
+++ Assets/lightadjust1.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightadjust1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Light light;
+    public float minIntensity = 0.0f;
+    public float maxIntensity = 6.7f;
+    public float frequency = 0.01f;
+    
+
+    void Start()
+    {
+
+        light = GetComponent<Light>();
+        InvokeRepeating("intensity", 2,1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float x = (Time.time + phase) * frequency;
+        //x = x-Mathf.Floor(x); // normalized value to 0..1
+        //light.intensity = maxIntensity * Mathf.Sin(2 * Mathf.PI * x) + minIntensity;
+
+        
+
+
+        //light.intensity = minIntensity + frequency;
+        //frequency++;
+        //if(maxIntensity==6.7f)
+        //{
+
+        //}
+
+        
+    }
+
+    void intensity()
+    {
+        light.intensity = minIntensity + frequency;
+        frequency++;
+        if (light.intensity == 6.7f)
+        {
+            light.intensity = 0.0f;
+            }
+    }
+
+
+
+
+}
