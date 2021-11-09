8bfd79f4e74cee696db3aee0b511246a92953bbc
u1372950 <u1372950@umail.utah.edu>
2021-11-08 18:20:45 -0700

    lighteffect

--- Assets/lightadjust.cs
+++ Assets/lightadjust.cs
@@ -6,40 +6,30 @@ public class lightadjust : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public Light mainlight;
-    public bool c=true;
-    
+    public Light mylight;
+
+    public bool changerange = false;
+    public float rangespeed = 1.0f;
+    public float maxrange = 10.0f;
+
+    public bool changeintensity = false;
+    public float intensityspeed = 1.0f;
+    public float maxintensity = 23.0f;
+
+    float starttime;
+
     void Start()
     {
-        Invoke("changeposition",2);
-        
+        mylight = GetComponent<Light>();
+        starttime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    void changeposition()
-    {
-        
-        
-        if(c)
+        if (changeintensity)
         {
-            this.transform.position = new Vector3(0.555999994f, -5.75f, -0.356999993f);
+            mylight.intensity = Mathf.PingPong(Time.time * intensityspeed, maxintensity);
         }
-
-        //if(c!)
-        //{
-        //    this.transform.position = new Vector3(0.555999994f, 1.30700004f, -0.356999993f);
-        //    c = false;
-        //}
-        
-        //this.transform.position = new Vector3(0.555999994f, -5.75f, -0.356999993f);
-        //this.transform.position = new Vector3(0.555999994f, 1.30700004f, -0.356999993f);
-        //InvokeRepeating("change", 0, 2);
     }
-
-    
 }
